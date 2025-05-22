47b03f4288751b399a9d5c4a06dc0555b17f9ab9
varavuth <ton_max_it@hotmail.com>
2025-05-22 15:53:48 +0700

    EVAR-13 <message>

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Jira 3.0.0 New</h1>
+      <h1>Vite + React + Jira 6.0.1 production</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
